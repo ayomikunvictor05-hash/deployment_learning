c8e235448c097ade6edd4049cf3cd6af591c71c3
Ayomikun Victor <ayomikunvictor05@gmail.com>
2026-05-07 11:59:04 -0800

    added staticfiles

--- companyProject/settings.py
+++ companyProject/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w)p7)i63wrand45ax-09#je=r@tx2%&00l^d(fhawo6=nwl9%3"
-
+# SECRET_KEY = "django-insecure-w)p7)i63wrand45ax-09#je=r@tx2%&00l^d(fhawo6=nwl9%3"
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
